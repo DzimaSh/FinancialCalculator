4aeff9a68abc64d116ecba06ee8ad12cec8ea07c
DzimaSh <dzmitry.shushkevich@gmail.com>
2023-10-12 01:40:58 +0200

    accept both , and . separators

--- src/utils/Parser.java
+++ src/utils/Parser.java
@@ -2,15 +2,23 @@ package utils;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.ParseException;
+import java.util.Locale;
 
 public class Parser {
+    private static final DecimalFormat decimalFormat;
+
+    static {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.of("en"));
+        decimalFormat = new DecimalFormat("#.##########", symbols);
+    }
     private static final BigDecimal MIN_VALUE = new BigDecimal("-1000000000000.000000");
     private static final BigDecimal MAX_VALUE = new BigDecimal("1000000000000.000000");
 
     public static BigDecimal parseValue(String number) throws ParseException {
-        BigDecimal parsedValue = new BigDecimal(number);
+        String normalizedNumber = number.replace(',', '.');
+        BigDecimal parsedValue = new BigDecimal(decimalFormat.parse(normalizedNumber).toString());
 
         if (parsedValue.compareTo(MIN_VALUE) < 0 || parsedValue.compareTo(MAX_VALUE) > 0) {
             throw new ParseException("Parsed value is out of range", 0);
